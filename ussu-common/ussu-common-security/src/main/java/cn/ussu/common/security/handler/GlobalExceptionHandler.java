d188ad2cf412c3a91b65449f503403e73419c850
liming <useeseeu@163.com>
2021-07-13 14:03:10 +0800

    统一返回包装

--- ussu-common/ussu-common-security/src/main/java/cn/ussu/common/security/handler/GlobalExceptionHandler.java
+++ ussu-common/ussu-common-security/src/main/java/cn/ussu/common/security/handler/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package cn.ussu.common.security.handler;
 
+import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.ussu.common.core.constants.ErrorMsgConstants;
-import cn.ussu.common.core.model.vo.JsonResult;
 import cn.ussu.common.core.exception.UsernamePasswordInvalidException;
+import cn.ussu.common.core.model.vo.JsonResult;
 import cn.ussu.common.security.exception.PermCheckException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
 
 /**
  * 异常处理
@@ -30,12 +33,28 @@ public class GlobalExceptionHandler {
     }
 
     /**
+     * 断言异常
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    public JsonResult handleIllegalArgumentException(IllegalArgumentException illegalArgumentException) {
+        return JsonResult.error(illegalArgumentException.getMessage());
+    }
+
+    /**
+     * sql异常
+     */
+    @ExceptionHandler(SQLException.class)
+    public JsonResult handleSQLException(SQLException sqlException) {
+        return JsonResult.error(sqlException.getMessage()).put("sqlState", sqlException.getSQLState());
+    }
+
+    /**
      * 其他运行时异常
      */
     @ExceptionHandler(RuntimeException.class)
     public JsonResult handleRuntimeException(RuntimeException runtimeException) {
         runtimeException.printStackTrace();
-        return JsonResult.error(runtimeException.getMessage()).put("exception", runtimeException.getStackTrace().toString());
+        return JsonResult.error(runtimeException.getMessage()).put("exception", ExceptionUtil.stacktraceToString(runtimeException));
     }
 
 }
