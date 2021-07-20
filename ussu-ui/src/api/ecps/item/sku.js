0990164337aee6371f87546d854f2f0139681487
liming <useeseeu@163.com>
2021-07-20 19:28:23 +0800

    bak

---
+++ ussu-ui/src/api/ecps/item/sku.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export function getList(query) {
+  return request({
+    url: '/item/sku',
+    method: 'get',
+    params: query
+  })
+}
+
+export function detail(id) {
+  return request({
+    url: '/item/sku/' + id,
+    method: 'get'
+  })
+}
+
+export function add(data) {
+  return request({
+    url: '/item/sku',
+    method: 'put',
+    data
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: '/item/sku',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: '/item/sku/' + ids,
+    method: 'delete'
+  })
+}
+
