8235a5550b69c3f00f59ffb2bb656889a628640e
liming <useeseeu@163.com>
2023-08-25 13:31:41 +0800

    test

---
+++ ussu-modules/ussu-modules-test/src/main/java/cc/ussu/modules/test/TestApplication.java
@@ -0,0 +1,13 @@
+package cc.ussu.modules.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class);
+    }
+
+}
