959d6b9e1c2b3047fdc097a6698f3d6085e205b2
liming <useeseeu@163.com>
2023-09-11 18:25:18 +0800

    application update

--- ussu-modules/ussu-modules-test/src/main/java/cc/ussu/modules/test/TestApplication.java
+++ ussu-modules/ussu-modules-test/src/main/java/cc/ussu/modules/test/TestApplication.java
@@ -2,12 +2,15 @@ package cc.ussu.modules.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class);
+        SpringApplication app = new SpringApplication(TestApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
 
 }
